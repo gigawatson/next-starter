495a5e46ac3a04ecfb1523d2c3504c5975f4c43e
Exploriter <67083853+gigawatson@users.noreply.github.com>
2025-07-20 13:41:38 -0700

    Init

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+// Create a single Prisma client instance globally in dev
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+	globalForPrisma.prisma = prisma;
+}
